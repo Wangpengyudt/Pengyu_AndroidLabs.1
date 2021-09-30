b4b50e93bd100f7207653239138b2f32b313ab3a
wangpengyu <wangpengyudt@gmail.com>
2021-09-30 10:52:49 -0400

    Version of Lab 3

--- app/src/main/java/algonquin/cst2355/wang0532/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/wang0532/MainActivity.java
@@ -5,72 +5,108 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    ImageView imgView;
+    Switch sw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mytext = findViewById(R.id.textview);
-        Button myButton = findViewById(R.id.mybutton);
-        EditText myedit = findViewById(R.id.myedittext);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String editString = myedit.getText().toString();
-                mytext.setText("Your edit text has:" + editString);
+
+        imgView = findViewById(R.id.flagview);
+        sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+
+            if (isChecked)
+            {
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+
+                imgView.startAnimation(rotate);
             }
+            else {
+                imgView.clearAnimation();
+            }
+
         });
-
-        Button myCheckBoxButton1 = findViewById(R.id.myCheckBoxbutton1);
-        myCheckBoxButton1.setText("CheckBox1");
-        myCheckBoxButton1.setOnClickListener(   vw  ->  mytext.setText("CheckBox1"));
-
-
-        Button myCheckBoxButton2 = findViewById(R.id.myCheckBoxbutton2);
-        myCheckBoxButton2.setText("CheckBox2");
-        myCheckBoxButton2.setOnClickListener(   vw  ->  mytext.setText("CheckBox2"));
-
-        Button mySwitch1 = findViewById(R.id.mySwitch1);
-        mySwitch1.setText("Switch 1");
-        mySwitch1.setOnClickListener(   vw  ->  mytext.setText("Switch1"));
-
-        Button mySwitch2 = findViewById(R.id.mySwitch2);
-        mySwitch2.setText("Switch 2");
-        mySwitch2.setOnClickListener(   vw  ->  mytext.setText("Switch2"));
-
-        Button option1 = findViewById(R.id.myRadioButton1);
-        option1.setText("Option1");
-        option1.setOnClickListener(   vw  ->  mytext.setText("Option1"));
-
-        Button option2 = findViewById(R.id.myRadioButton2);
-        option2.setText("Option2");
-        option2.setOnClickListener(   vw  ->  mytext.setText("Option2"));
-
-        Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
-        ImageView myimage = findViewById(R.id.myImage);
-
-        ImageButton imgbtn = findViewById( R.id.myImagebutton );
-
-
-
-        Context context1 = getApplicationContext();
-        CharSequence text1 = "The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight();
-        imgbtn.setOnClickListener(   vw  ->  mytext.setText("The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight()));
-        int duration1 = Toast.LENGTH_LONG;
-
     }
+
 }
+
+
+//        TextView mytext = findViewById(R.id.textview);
+//        Button myButton = findViewById(R.id.mybutton);
+//        EditText myedit = findViewById(R.id.myedittext);
+//        myButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String editString = myedit.getText().toString();
+//                mytext.setText("Your edit text has:" + editString);
+//            }
+//        });
+//
+//
+//
+//
+//        Button myCheckBoxButton1 = findViewById(R.id.myCheckBoxbutton1);
+//        myCheckBoxButton1.setText("CheckBox1");
+//        myCheckBoxButton1.setOnClickListener(   vw  ->  mytext.setText("CheckBox1"));
+//
+//
+//        Button myCheckBoxButton2 = findViewById(R.id.myCheckBoxbutton2);
+//        myCheckBoxButton2.setText("CheckBox2");
+//        myCheckBoxButton2.setOnClickListener(   vw  ->  mytext.setText("CheckBox2"));
+//
+//        Button mySwitch1 = findViewById(R.id.mySwitch1);
+//        mySwitch1.setText("Switch 1");
+//        mySwitch1.setOnClickListener(   vw  ->  mytext.setText("Switch1"));
+//
+//        Button mySwitch2 = findViewById(R.id.mySwitch2);
+//        mySwitch2.setText("Switch 2");
+//        mySwitch2.setOnClickListener(   vw  ->  mytext.setText("Switch2"));
+//
+//        Button option1 = findViewById(R.id.myRadioButton1);
+//        option1.setText("Option1");
+//        option1.setOnClickListener(   vw  ->  mytext.setText("Option1"));
+//
+//        Button option2 = findViewById(R.id.myRadioButton2);
+//        option2.setText("Option2");
+//        option2.setOnClickListener(   vw  ->  mytext.setText("Option2"));
+//
+//        Context context = getApplicationContext();
+//        CharSequence text = "Hello toast!";
+//        int duration = Toast.LENGTH_LONG;
+//
+//        Toast toast = Toast.makeText(context, text, duration);
+//        toast.show();
+//
+//        ImageView myimage = findViewById(R.id.myImage);
+//
+//        ImageButton imgbtn = findViewById( R.id.myImagebutton );
+//
+//
+//
+//        Context context1 = getApplicationContext();
+//        CharSequence text1 = "The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight();
+//        imgbtn.setOnClickListener(   vw  ->  mytext.setText("The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight()));
+//        int duration1 = Toast.LENGTH_LONG;
+//
+//    }
+//}
