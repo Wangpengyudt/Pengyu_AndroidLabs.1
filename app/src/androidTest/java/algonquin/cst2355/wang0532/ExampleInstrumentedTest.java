247049a3a9c29c497794b95881c6bde2ae0a4e96
wangpengyu <wangpengyudt@gmail.com>
2021-09-30 22:45:57 -0400

    week3

--- app/src/androidTest/java/algonquin/cst2355/wang0532/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/algonquin/cst2355/wang0532/ExampleInstrumentedTest.java
@@ -12,7 +12,7 @@ import static org.junit.Assert.*;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *week3
+ *week3 commit
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
