34a131d60dda412e3002afcc5e7c31be2654b676
wangpengyu <wangpengyudt@gmail.com>
2021-10-15 23:06:39 -0400

    finished week 5

--- app/src/main/java/algonquin/cst2355/wang0532/ChatRoom.java
+++ app/src/main/java/algonquin/cst2355/wang0532/ChatRoom.java
@@ -1,46 +1,194 @@
 package algonquin.cst2355.wang0532;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import com.google.android.material.snackbar.Snackbar;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.Locale;
 
 public class ChatRoom extends AppCompatActivity {
+    ArrayList<ChatMessage> messages = new ArrayList<>();
 
-    RecyclerView chatList;
+    Button buttonSend;
+    Button buttonReceive;
+    EditText edit;
+    RecyclerView recyclerView;
+    MyAdapter theAdapter;
+
+
+
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState){
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState){
+        super.onRestoreInstanceState(savedInstanceState);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        //load XML:
         setContentView( R.layout.chatlayout );
-        chatList = findViewById(R.id.myrecycler);
-        chatList.setAdapter(new MyChatAdapter());
+
+        buttonSend = findViewById(R.id.buttonSend);  //send button
+
+        buttonReceive = findViewById(R.id.buttonReceive); //receive button
+
+        edit = findViewById(R.id.editTextTextPersonName);  //edit text
+
+        recyclerView = findViewById(R.id.myrecycler);  //recycler view
+
+        buttonSend.setOnClickListener( click ->{
+            String whatIsTyped = edit.getText().toString();
+            Date timeNow = new Date(); //get time run
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault());
+
+            String currentDateandTime = sdf.format(timeNow);   //convert date to string
+
+            messages.add(new ChatMessage(whatIsTyped, currentDateandTime));
+            edit.setText("");
+            theAdapter.notifyItemInserted(messages.size() - 1);
+            recyclerView.setAdapter(theAdapter);
+            recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        });
+        theAdapter = new MyAdapter();
+        recyclerView.setAdapter(theAdapter);
+        StaggeredGridLayoutManager layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 
-    private class MyChatAdapter extends RecyclerView.Adapter{
+        public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
 
-        @Override
-        public  RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
-            return null;
+            @Override
+            public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                //load a new row from the layout file
+                LayoutInflater li =  getLayoutInflater();
+                View thisRow;
+                //import layout for a row
+
+                thisRow = li.inflate(R.layout.sent_message, parent, false);
+
+
+                return new MyViewHolder( thisRow );
+            }
+            //initialize a row at position
+            @Override
+            public void onBindViewHolder(MyViewHolder holder, int position) {   //need an arraylist to hold all the message.
+                ChatMessage thisRow = messages.get(position);
+
+                holder.timeView.setText(thisRow.getTimeSent()); //what time goes on row position
+                holder.messageView.setText( thisRow.getMessage()); //what message goes on row position
+
+
+            }
+
+
+            @Override
+            public int getItemCount() {
+                return messages.size();
+            }
         }
 
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position){
 
-        }
 
-        @Override
-        public  int getItemCount(){
-            return 0;
+    //this holds textViews on a row
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView timeView;
+        TextView messageView;
+
+        public MyViewHolder(View itemView){
+        super(itemView);
+
+        itemView.setOnClickListener( click -> {
+            int position = getAbsoluteAdapterPosition();
+            ChatMessage whatWasClicked = messages.get(position);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder( ChatRoom.this);
+
+
+            builder.setTitle("Question:")
+                   .setMessage("Do you want to delete this:" + whatWasClicked.getMessage())
+                    .setNegativeButton("Negative", (dialog, e)->{})
+                    .setPositiveButton("Positive", (dialog, e2)->{
+                        messages.remove(position);
+                        theAdapter.notifyItemRemoved(position);
+                        Snackbar.make(buttonSend, "You removed item #" + position, Snackbar.LENGTH_LONG).show();
+                    })
+                    .setNeutralButton("Neutral", (dialog, e3)->{}).create().show();
+
+
+        });
+
+        timeView = itemView.findViewById(R.id.time);
+        messageView = itemView.findViewById(R.id.message);
+    }
+    }
+
+
+    private class MyRowViews extends RecyclerView.ViewHolder{
+
+        TextView messageText;
+        TextView timeText;
+
+        public MyRowViews(View itemView) {
+            super(itemView);
+
+
         }
     }
+
+
     private  class ChatMessage{
         String message;
-        int sendOrReceive;
-        Date timeSent;
+       // String receive;
+        String timeSent;
+
+        public String getMessage() {
+            return message;
+        }
+
+        //public String getReceive(){return receive;}
+
+        public String getTimeSent() {
+
+            return timeSent;
+        }
+
+        public ChatMessage(String message, String timeSent) {
+            this.message = message;
+            //this.receive = receive;
+            this.timeSent = timeSent;
+
+
+        }
+
+
+
     }
 }
--- app/src/main/java/algonquin/cst2355/wang0532/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/wang0532/MainActivity.java
@@ -18,59 +18,59 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView mytext = findViewById(R.id.textview);
-        Button myButton = findViewById(R.id.mybutton);
-        EditText myedit = findViewById(R.id.myedittext);
-        myButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String editString = myedit.getText().toString();
-                mytext.setText("Your edit text has:" + editString);
-            }
-        });
-
-        Button myCheckBoxButton1 = findViewById(R.id.myCheckBoxbutton1);
-        myCheckBoxButton1.setText("CheckBox1");
-        myCheckBoxButton1.setOnClickListener(   vw  ->  mytext.setText("CheckBox1"));
-
-
-        Button myCheckBoxButton2 = findViewById(R.id.myCheckBoxbutton2);
-        myCheckBoxButton2.setText("CheckBox2");
-        myCheckBoxButton2.setOnClickListener(   vw  ->  mytext.setText("CheckBox2"));
-
-        Button mySwitch1 = findViewById(R.id.mySwitch1);
-        mySwitch1.setText("Switch 1");
-        mySwitch1.setOnClickListener(   vw  ->  mytext.setText("Switch1"));
-
-        Button mySwitch2 = findViewById(R.id.mySwitch2);
-        mySwitch2.setText("Switch 2");
-        mySwitch2.setOnClickListener(   vw  ->  mytext.setText("Switch2"));
-
-        Button option1 = findViewById(R.id.myRadioButton1);
-        option1.setText("Option1");
-        option1.setOnClickListener(   vw  ->  mytext.setText("Option1"));
-
-        Button option2 = findViewById(R.id.myRadioButton2);
-        option2.setText("Option2");
-        option2.setOnClickListener(   vw  ->  mytext.setText("Option2"));
-
-        Context context = getApplicationContext();
-        CharSequence text = "Hello toast!";
-        int duration = Toast.LENGTH_LONG;
-
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
-
-        ImageView myimage = findViewById(R.id.myImage);
-
-        ImageButton imgbtn = findViewById( R.id.myImagebutton );
-
-
-
-        Context context1 = getApplicationContext();
-        CharSequence text1 = "The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight();
-        imgbtn.setOnClickListener(   vw  ->  mytext.setText("The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight()));
-        int duration1 = Toast.LENGTH_LONG;
-
-    }
+//        TextView mytext = findViewById(R.id.textview);
+//        Button myButton = findViewById(R.id.mybutton);
+//        EditText myedit = findViewById(R.id.myedittext);
+//        myButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                String editString = myedit.getText().toString();
+//                mytext.setText("Your edit text has:" + editString);
+//            }
+//        });
+//
+//        Button myCheckBoxButton1 = findViewById(R.id.myCheckBoxbutton1);
+//        myCheckBoxButton1.setText("CheckBox1");
+//        myCheckBoxButton1.setOnClickListener(   vw  ->  mytext.setText("CheckBox1"));
+//
+//
+//        Button myCheckBoxButton2 = findViewById(R.id.myCheckBoxbutton2);
+//        myCheckBoxButton2.setText("CheckBox2");
+//        myCheckBoxButton2.setOnClickListener(   vw  ->  mytext.setText("CheckBox2"));
+//
+//        Button mySwitch1 = findViewById(R.id.mySwitch1);
+//        mySwitch1.setText("Switch 1");
+//        mySwitch1.setOnClickListener(   vw  ->  mytext.setText("Switch1"));
+//
+//        Button mySwitch2 = findViewById(R.id.mySwitch2);
+//        mySwitch2.setText("Switch 2");
+//        mySwitch2.setOnClickListener(   vw  ->  mytext.setText("Switch2"));
+//
+//        Button option1 = findViewById(R.id.myRadioButton1);
+//        option1.setText("Option1");
+//        option1.setOnClickListener(   vw  ->  mytext.setText("Option1"));
+//
+//        Button option2 = findViewById(R.id.myRadioButton2);
+//        option2.setText("Option2");
+//        option2.setOnClickListener(   vw  ->  mytext.setText("Option2"));
+//
+//        Context context = getApplicationContext();
+//        CharSequence text = "Hello toast!";
+//        int duration = Toast.LENGTH_LONG;
+//
+//        Toast toast = Toast.makeText(context, text, duration);
+//        toast.show();
+//
+//        ImageView myimage = findViewById(R.id.myImage);
+//
+//        ImageButton imgbtn = findViewById( R.id.myImagebutton );
+//
+//
+//
+//        Context context1 = getApplicationContext();
+//        CharSequence text1 = "The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight();
+//        imgbtn.setOnClickListener(   vw  ->  mytext.setText("The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight()));
+//        int duration1 = Toast.LENGTH_LONG;
+//
+   }
 }
